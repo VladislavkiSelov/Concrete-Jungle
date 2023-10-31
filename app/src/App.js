d6fa0ba71c1fe18aaa549c24ec2917f9428a3eeb
[VladislavkiSelov] <[vladislavkiselev.2007@gmail.com]>
2023-10-31 23:04:52 +0200

    feat: header

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,20 @@
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer"
-import MainPageSection1 from './components/MainPageSection1/MainPageSection1'
+import "./style/reset.scss";
+import "./style/fonts.scss";
 import "./style/index.scss";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import MainPageSection1 from "./components/MainPageSection1/MainPageSection1";
 
 function App() {
-  return <div className="App wrapper">
+  return (
+    <div className="App wrapper">
       <Header />
       <div className="Main">
         <MainPageSection1 />
       </div>
       <Footer />
-    </div>;
+    </div>
+  );
 }
 
 export default App;
--- app/src/components/MainPageSection1/MainPageSection1.jsx
+++ app/src/components/MainPageSection1/MainPageSection1.jsx
@@ -4,7 +4,6 @@ import './MainPageSection1.scss'
 export default function MainPageSection1() {
   return (
     <div>
-      <button className='m-10 text-red-600 bg-black'>Send</button>
     </div>
   )
 }
