3816060af8d3571164ef8755b38cdb39ed0b39be
VladislavkiSelov <vladislavkiselev.2007@gmail.com>
2023-11-04 23:00:16 +0200

    add validation Calculator

--- app/src/components/Calculator/Calculator.jsx
+++ app/src/components/Calculator/Calculator.jsx
@@ -1,47 +1,90 @@
 import React from "react";
-import {Button} from "../Button/Button";
-import { Radio, Slider,ConfigProvider } from "antd";
+import { Button } from "../Button/Button";
+import { Radio, Slider, ConfigProvider } from "antd";
 import { useForm } from "react-hook-form";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { useIMask } from "react-imask";
+import { IMaskInput } from 'react-imask';
 import './Calculator.scss'
 
 export default function Calculator() {
+  const ref = useRef(null);
+  const inputRef = useRef(null);
+  const [isInputValid, setInputValid] = useState(false);
   const [valueTypeRepair, setValueTypeRepair] = useState("Косметичний");
   const [valueBuilding, setValueBuilding] = useState("Новобудова");
   const [valueNumberRoom, setValueNumberRoom] = useState(1);
+  const [valueSlider, setValueSlider] = useState(30);
+  const [phoneNumber, setPhoneNumber] = useState('')
   const [day, setDay] = useState(0);
   const [price, setPrice] = useState(0);
-  const [valueSlider, setValueSlider] = useState(30);
-  const validatePhone = /^\+380\d{3}\d{2}\d{2}\d{2}$/;
-  const { register, handleSubmit, reset, formState: { errors } } = useForm();
+  const { register, setValue, handleSubmit, reset, formState: { errors } } = useForm({
+    defaultValues: {
+      typeRepair: valueTypeRepair,
+      typeBuilding: valueBuilding,
+      numberRoom: valueNumberRoom,
+      square: valueSlider
+    }
+  });
+
+  const handleAccept = () => {
+    console.log('b');
+    setInputValid(false);
+  };
+
+  const handleEmpty = () => {
+    console.log('a');
+    setInputValid(true);
+  };
+
+  //  const { ref } = useIMask({});
+
 
   const onSubmitForm = data => {
-    console.log(data);
+    if (isInputValid) {
+      ref.current.element.classList.remove('error')
+        console.log(data);
+    } else {
+      ref.current.element.classList.add('error')
+      return
+    }
     reset();
   };
 
   const typeRepairOnChange = e => {
     setValueTypeRepair(e.target.value);
+    setValue('typeRepair', e.target.value)
   };
 
   const typeBuildingOnChange = e => {
     setValueBuilding(e.target.value);
+    setValue('typeBuilding', e.target.value)
   };
 
   const numberRoomOnChange = e => {
     setValueNumberRoom(e.target.value);
+    setValue('numberRoom', e.target.value)
+  };
+
+  const sliderSquare = value => {
+    setValueSlider(value)
+    setValue('square', value)
+  };
+
+  const handelChangePhone = e => {
+    setPhoneNumber(e.target.value)
+    setValue('phone', e.target.value)
   };
 
   return <form className="formCalculator" onSubmit={handleSubmit(onSubmitForm)}>
-      <ConfigProvider
-    theme={{
+    <ConfigProvider
+      theme={{
         components: {
           Slider: {
-            // colorPrimary: '#2d4872',
-            handleColor:'#2d4872',
-            handleActiveColor:'#2d4872',
-            trackBg:'#2d4872',
-            trackHoverBg:'#2d4872',
+            handleColor: '#2d4872',
+            handleActiveColor: '#2d4872',
+            trackBg: '#2d4872',
+            trackHoverBg: '#2d4872',
             dotActiveBorderColor: '#2d4872',
             colorPrimaryBorderHover: '#2d4872',
             dotBorderColor: '#2d4872',
@@ -51,24 +94,22 @@ export default function Calculator() {
             algorithm: true,
           },
           Radio: {
-            // colorPrimary: '#2d4872',
             colorWhite: '#2d4872',
             colorPrimary: '#2d4872',
             colorTextLightSolid: '#fff',
-      
             algorithm: true,
           }
         },
-    }}
-  >
-    <h2>Розрахуйте вартість ремонту</h2>
+      }}
+    >
+      <h2>Розрахуйте вартість ремонту</h2>
       <div className="typeRepair">
         <h2>Вид ремонту:</h2>
         <Radio.Group className="box_repair_radio" onChange={typeRepairOnChange} value={valueTypeRepair}>
           <Radio value={"Косметичний"}>Косметичний</Radio>
           <Radio value={"Капітальний"}>Капітальний</Radio>
           <Radio value={"Під ключ"}>Під ключ</Radio>
-            <Radio value={"Дизайнерський"}>Дизайнерський</Radio>
+          <Radio value={"Дизайнерський"}>Дизайнерський</Radio>
         </Radio.Group>
       </div>
       <div className="typeBuilding">
@@ -89,25 +130,43 @@ export default function Calculator() {
       </div>
       <div className="squareRoom">
         <h2>Площа, м2:</h2>
-        <Slider defaultValue={30} onChange={setValueSlider} tooltip={{ open: true }} value={valueSlider} />
-    </div>
-    <div className="sale_repaire">
-      <div>
-        <h3>Термін ремонту:</h3>
-        <h4>до {day} днів</h4>
+        <Slider defaultValue={30} onChange={(value) => sliderSquare(value)} tooltip={{ open: true }} value={valueSlider} />
       </div>
-      <div>
-        <h3>Знижка складе:</h3>
-        <h4>{price} грн</h4>
+      <div className="sale_repaire">
+        <div>
+          <h3>Термін ремонту:</h3>
+          <h4>до {day} днів</h4>
+        </div>
+        <div>
+          <h3>Знижка складе:</h3>
+          <h4>{price} грн</h4>
+        </div>
       </div>
-    </div>
       <div className="call">
-      <h2>Введите номер телефона</h2>
-      <div>
-          <input className={errors?.phone && 'error'} {...register("phone", { required: true, pattern: validatePhone })} placeholder='+(380)' />
-          <Button text='Замовити' buttonClass='order_btn'/>
+        <h2>Введите номер телефона</h2>
+        <div>
+
+          <IMaskInput
+            mask="+{7}(000)000-00-00"
+            radix="."
+            unmask={true} // true|false|'typed'
+            ref={ref}
+            inputRef={inputRef}
+            onAccept={
+              () => handleAccept()
+            }
+            onComplete={() => handleEmpty()}
+            className={errors?.phone && 'error'}
+            placeholder="+{7}(000)000-00-00"
+            onChange={(e) => handelChangePhone(e)} 
+          />
+          <Button text='Замовити' buttonClass='order_btn' />
+        </div>
       </div>
-      </div>
-      </ConfigProvider>
-    </form>;
+    </ConfigProvider>
+  </form>;
 }
+
+
+
+
